6a36550622818156f0a31eb0d9806af78b893640
Suyel Haque <slsuyel@gmail.com>
2024-03-28 23:37:43 +0600

    Remove dist and node_modules directories

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.GoToTop = void 0;
-const GoToTop_1 = __importDefault(require("./src/GoToTop"));
-exports.GoToTop = GoToTop_1.default;
